8d8556ab6315a1b377dad70387ecb5845799b37e
mathew evans <matevans1433@gmail.com>
2022-10-17 15:48:31 -0600

    added file structure for login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './loginportal.css';
+
+
+const Login = () => {
+  return (
+    <div className="text">
+      <h1>MKTFY</h1>
+      <h4>Buy and sell stuff!</h4>
+      <div>
+      <button>Login</button>
+      </div>
+      <div>
+      <button>Create Account</button>
+      </div>
+       <div>Find out more about! Visit our website</div>
+    </div>
+  );
+}
+
+export default Login;
