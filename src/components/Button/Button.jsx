4259ba824977fe40d167cb6bb0f8a5151d85c082
mathew evans <matevans1433@gmail.com>
2022-11-09 11:56:13 -0700

    complete btn-component and login layout

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Button.css";
+
+const STYLES = [
+  "btn--primary--solid",
+  "btn--primary--waiting",
+  "btn--secondary--solid",
+];
+
+const SIZES = ["btn--large", "btn--medium", "btn--small"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../../components/Button/Button.jsx";
 import "./login.css";
 import MKTFY from "../../assets/img/MKTFY.png";
 
@@ -7,17 +8,40 @@ import MKTFY from "../../assets/img/MKTFY.png";
 const Login = () => {
   return (
     <div className="login-container">
-      <div className="logo-container">
-        <img src={MKTFY} alt="mktfy logo" />
-      </div>
-      <div className="btn-login-container">
-        <button className="btn-login">Login</button>
-      </div>
-      <div className="btn-acc-container">
-        <button className="btn-cr-acc">Create Account</button>
+      <div className="flex-1 flex flex-col justify-center">
+        <div className="logo-container">
+          <img src={MKTFY} alt="mktfy logo" />
+        </div>
+        <div className="btn-login-container">
+          <Button
+            onClick={() => {
+              console.log("Clicked on");
+            }}
+            type="button"
+            buttonStyle="btn--secondary--solid"
+            buttonSize="btn--large"
+          >
+            Login
+          </Button>
+        </div>
+        <div className="btn-acc-container">
+          <Button
+            onClick={() => {
+              console.log("Clicked on");
+            }}
+            type="button"
+            buttonStyle="btn--primary--solid"
+            buttonSize="btn--large"
+          >
+            Create Account
+          </Button>
+        </div>
       </div>
       <div className="web-link-container">
-        <p>Find out more about! Visit our website</p>
+        <p>
+          Find out more about!
+          <a href="https://launchpadbyvog.com/"> Visit our website</a>
+        </p>
       </div>
     </div>
   );
